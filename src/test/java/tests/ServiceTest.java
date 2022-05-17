3ef407417cce5d637b3dd8718e1a61a561f1439a
MariaBychik <mariabychik@gmail.com>
2022-05-17 12:17:46 +0300

    Added ServicePage and ServiceTest

--- src/test/java/tests/ServiceTest.java
+++ src/test/java/tests/ServiceTest.java
@@ -10,9 +10,9 @@ import pages.ServicePage;
 @Listeners({TestListener.class})
 public class ServiceTest extends BaseTest {
 
-    @Test(priority = 2,description="Search for services with valid values")
+    @Test(priority = 0,description="Search for services for customers with valid values")
     @Description("Test Description: Search for services for customers and performers")
-    public void validOrder() throws Exception {
+    public void validSearchOrder() throws Exception {
 
         ServicePage searchOrder = new ServicePage(driver);
         SoftAssert Assert = new SoftAssert();
@@ -22,11 +22,20 @@ public class ServiceTest extends BaseTest {
                 .filterCustomersOrder(getTestData("order"))
                 .selectCustomersOrder();
         Assert.assertTrue(searchOrder.getCustomerOrderType(), "Order is not found");
-        searchOrder.switchToPerformersTab()
+    }
+
+
+        @Test(priority = 1,description="Search for services for customers with valid values")
+        @Description("Test Description: Search for services for customers and performers")
+        public void validSearchPerformer() throws Exception {
+
+            ServicePage searchPerformer = new ServicePage(driver);
+            SoftAssert Assert = new SoftAssert();
+            searchPerformer.switchToPerformersTab()
                 .selectPerformersOrdersSection(getTestData("sectionPerformers"))
                 .filterPerformersOrder(getTestData("orderPerformers"))
                 .selectPerformersOrder();
-        Assert.assertTrue(searchOrder.getPerformerOrderType(), "Order is not found");
+        Assert.assertTrue(searchPerformer.getPerformerOrderType(), "Order is not found");
         Assert.assertAll();
     }
 }
